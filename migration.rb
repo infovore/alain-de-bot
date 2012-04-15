69d020dceb09c1226da206c8f366ace8e2de8ddf
Tom Armitage <tom@infovore.org>
2012-04-16 00:16:50 +0100

    Sqlite migration.

---
+++ migration.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+require "rubygems"
+require "bundler/setup"
+Bundler.require(:default)
+
+DB = Sequel.sqlite('alain_de_bot.db')
+
+DB.create_table :existing_tweets do
+  primary_key :id
+  String :content, :null => false
+  DateTime :created_at
+  index :created_at
+end
+
+DB.create_table :to_be_posted do
+  primary_key :id
+  String :content, :null => false
+  DateTime :created_at
+  index :created_at
+end
+
+
