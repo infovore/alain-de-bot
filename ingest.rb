c0d51b289e302730bc8a9df6ebd60f76a2347e6c
Tom Armitage <tom@infovore.org>
2012-04-16 00:16:35 +0100

    Ingest script.

---
+++ ingest.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+require "rubygems"
+require "bundler/setup"
+Bundler.require(:default)
+
+PATH_PREFIX = File.expand_path(File.dirname(__FILE__))
+
+config = YAML.load(File.read(PATH_PREFIX + "/creds.yml"))
+
+DB = Sequel.sqlite('alain_de_bot.db')
+
+%w{consumer_key consumer_secret access_token access_token_secret}.each do |key|
+  Object.const_set(key.upcase, config["config"][key])
+end
+
+Twitter.configure do |config|
+  config.consumer_key = CONSUMER_KEY
+  config.consumer_secret = CONSUMER_SECRET
+  config.oauth_token = ACCESS_TOKEN
+  config.oauth_token_secret = ACCESS_TOKEN_SECRET
+end
+
+user_name = 'alaindebotton' 
+tweets = [] 
+
+(1..16).each do |page| 
+  Twitter.user_timeline(user_name, :page => page, :count => 200).each do |tweet| 
+    tweets << tweet 
+  end 
+end 
+
+tweets.each do |tweet|
+  DB[:existing_tweets].insert(:id => tweet.id,
+                              :content => tweet.text)
+end
